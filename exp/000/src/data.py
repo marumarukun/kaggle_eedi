ec3e9da1f59ff7d862096d3e1232f780dc486aa4
Masato Ueda <cheriera.0310@gmail.com>
2024-11-09 23:34:50 +0900

    trainデータの前処理関数実装

--- exp/000/src/data.py
+++ exp/000/src/data.py
@@ -64,7 +64,7 @@ def add_subject_name_info(train_df: pl.DataFrame, mapping_df: pl.DataFrame) -> p
     # mappingデータの整形
     mapping_meta_df = mapping_meta_df.with_columns(
         [
-            # 複数のSubjectNamesがある場合は、それらを列挙した文字列を生成
+            # SubjectNamesがある場合は、それらを列挙した文字列を生成
             pl.when(pl.col("SubjectNames").list.len() > 0)
             .then(
                 pl.lit("The misconception '")
@@ -72,7 +72,7 @@ def add_subject_name_info(train_df: pl.DataFrame, mapping_df: pl.DataFrame) -> p
                 + pl.lit("' is primarily observed in the following subjects: ")
                 + pl.col("SubjectNames").list.join(", ")
             )
-            # 複数のSubjectNamesがない場合は、MisconceptionNameをそのまま使う
+            # SubjectNamesがない場合は、MisconceptionNameをそのまま使う
             .otherwise(pl.lit("The misconception is: ") + pl.col("MisconceptionName"))
             .alias("MisconceptionName_with_SubjectNames")
         ]
@@ -80,5 +80,71 @@ def add_subject_name_info(train_df: pl.DataFrame, mapping_df: pl.DataFrame) -> p
     return mapping_meta_df
 
 
-# TODO：train or testの整形関数実装
-# def preprocess_train_or_test
+# trainの前処理関数実装
+def preprocess_train(train_df: pl.DataFrame) -> pl.DataFrame:
+    common_col = [
+        "QuestionId",
+        "ConstructName",
+        "SubjectName",
+        "QuestionText",
+        "CorrectAnswer",
+    ]
+
+    train_long = (
+        train_df.select(pl.col(common_col + [f"Answer{alpha}Text" for alpha in ["A", "B", "C", "D"]]))
+        .unpivot(
+            index=common_col,
+            variable_name="AnswerType",
+            value_name="AnswerText",
+        )
+        .with_columns(
+            (
+                pl.lit("ConstructName: ")
+                + pl.col("ConstructName")
+                + pl.lit(" SubjectName: ")
+                + pl.col("SubjectName")
+                + pl.lit(" QuestionText: ")
+                + pl.col("QuestionText")
+                + pl.lit(" AnswerText: ")
+                + pl.col("AnswerText")
+            ).alias("AllText"),
+            pl.col("AnswerType").str.extract(r"Answer([A-D])Text$").alias("AnswerAlphabet"),
+        )
+        .with_columns(
+            pl.concat_str(
+                [
+                    pl.col("QuestionId"),
+                    pl.col("AnswerAlphabet"),
+                ],
+                separator="_",
+            ).alias("QuestionId_Answer"),
+        )
+        .sort("QuestionId_Answer")
+    )
+    train_misconception_long = (
+        train_df.select(pl.col(common_col + [f"Misconception{alpha}Id" for alpha in ["A", "B", "C", "D"]]))
+        .unpivot(
+            index=common_col,
+            variable_name="MisconceptionType",
+            value_name="MisconceptionId",
+        )
+        .with_columns(
+            pl.col("MisconceptionType").str.extract(r"Misconception([A-D])Id$").alias("AnswerAlphabet"),
+        )
+        .with_columns(
+            pl.concat_str([pl.col("QuestionId"), pl.col("AnswerAlphabet")], separator="_").alias("QuestionId_Answer"),
+        )
+        .sort("QuestionId_Answer")
+        .select(pl.col(["QuestionId_Answer", "MisconceptionId"]))
+        .with_columns(pl.col("MisconceptionId").cast(pl.Int64))
+    )
+    # join MisconceptionId
+    train_long = train_long.join(train_misconception_long, on="QuestionId_Answer")
+
+    # CorrectAnswerとAnswerAlphabetが一致するもの（つまり正解）は除外
+    # また、MisconceptionIdがNoneのものも除外
+    train_long = train_long.filter(pl.col("CorrectAnswer") != pl.col("AnswerAlphabet")).drop_nulls(
+        subset=["MisconceptionId"]
+    )
+
+    return train_long
